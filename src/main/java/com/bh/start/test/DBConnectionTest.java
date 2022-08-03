b11a50a415b0f84e4039e43f6116f03248cff3e9
5sbread <oohogong@gmail.com>
2022-08-03 11:11:36 +0900

    Test

--- src/main/java/com/bh/start/test/DBConnectionTest.java
+++ src/main/java/com/bh/start/test/DBConnectionTest.java
@@ -1,10 +1,19 @@
 package com.bh.start.test;
 
+import java.sql.Connection;
+
+import com.bh.start.util.DBConnector;
+
 public class DBConnectionTest {
 
 	public static void main(String[] args) {
 
-
+		try {
+			DBConnector.getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
